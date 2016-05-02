ca8f24d928a15122c97a3a0ee00ca5edc6eb7e74
Jesse Hoogervorst <hoogervorstjesse@gmail.com>
2016-05-02 13:35:15 +0200

    Initial Commit

---
+++ writingAid/main.c
@@ -0,0 +1,21 @@
+/*
+ * writingAid.c
+ *
+ * Created: 2-5-2016 13:31:53
+ * Author : Jesse
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
