bbe7af5ce97a6597925b96d8025345f5d68210c2
wbraakman <ward.braakman@gmail.com>
2016-05-04 14:24:59 +0200

    Created base files

---
+++ writingAid/src/Kinematics.c
@@ -0,0 +1,9 @@
+/*
+ * Kinematics.c
+ *
+ * Created: 4-5-2016 14:23:21
+ *  Author: wardb
+ */ 
+
+#include "Kinematics.h"
+
---
+++ writingAid/src/Kinematics.h
@@ -0,0 +1,16 @@
+/*
+ * Kinematics.h
+ *
+ * Created: 4-5-2016 14:23:39
+ *  Author: wardb
+ */ 
+
+
+#ifndef KINEMATICS_H_
+#define KINEMATICS_H_
+
+
+
+
+
+#endif /* KINEMATICS_H_ */
